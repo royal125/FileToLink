5c677b16be14fd2827f951c4f006b6fa6febbd34
Harinderpal singh <hsbhular@gmail.com>
2025-08-29 20:53:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '23323985'))
+API_HASH = environ.get('API_HASH', 'd24809282e7c046a98a04ca3c66659e7')
+BOT_TOKEN = environ.get('BOT_TOKEN', "8446506316:AAEw2EILjQqj4mT4PNygZPEZRw950ovhfIU")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -23,12 +23,12 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002986443155'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://falvolison_db_user:VyMuDNsnRu5szE56@cluster0.ijcfo7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "falvolison_db_user")
 
 # Shortlink Info
 SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
